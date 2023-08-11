6b4efe3e3c9256699e244021d2f98fdec66b31ec
HaraT0323 <alra9si916323@gmail.com>
2023-08-11 12:34:15 +0900

    DB周りの実装

---
+++ migrate/migrate.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"go-echo-api/db"
+	"go-echo-api/model"
+)
+
+func main() {
+	dbConn := db.NewDB()
+	defer fmt.Println("Successfully Migrated")
+	defer db.CloseDB(dbConn)
+	dbConn.AutoMigrate(&model.User{}, &model.Task{})
+}
